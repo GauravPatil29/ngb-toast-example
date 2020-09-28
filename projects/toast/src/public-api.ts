f2403f40fb087424c5cd48f8319e8fc00d3683af
Gaurav Patil <gauravpatil2994@gmail.com>
2020-09-28 14:33:00 +0530

    ngb-toast code removed

--- projects/toast/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of toast
- */
-
-export * from './lib/interface/ngb-toast';
-export * from './lib/provider/ngb-toast.service';
-export * from './lib/pages/ngb-toast-container';
-export * from './lib/ngb-toast.module';
